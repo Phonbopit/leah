bd0d376cb5a51212e0fee33ef13f89184a0b6d6a
Chai Phonbopit <phonbopit@gmail.com>
2016-05-30 21:50:57 +0700

    update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,30 @@
-'use strict';
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  entry: './src/App.jsx',
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/client?reload=true',
+    './public/app/Main.jsx'
+  ],
   output: {
-    path: './dist',
-    filename: 'bundle.js'
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
   },
-  devServer: {
-    contentBase: './dist',
-    port: 5555
-  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
   module: {
     loaders: [{
       test: /\.jsx?$/,
-      exclude: /node_modules/,
       loader: 'babel',
+      exclude: /node_modules/,
       query: {
-        presets: ['es2015', 'react']
+        presets: ['es2015', 'react', 'stage-0']
       }
     }]
-  },
-  devtool: 'cheap-module-source-map',
-  resolve: {
-    extensions: ['', '.js', '.json', '.jsx']
   }
-}
+};
